6d5dc7959636cece1d16aab4dcb9823f40bc6c07
hoochicken <mareike.riegel@ql.de>
2023-05-07 18:26:59 +0200

    initial commit

---
+++ my-app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <v-row class="text-center">
+    <v-col cols="12">
+      <h1 class="display-2 font-weight-bold mb-3">
+        Randomizer
+      </h1>
+      <v-img
+          :src="require('./assets/elves.png')"
+          class="my-3"
+          contain
+          height="200"
+      />
+    </v-col>
+  </v-row>
+  <router-view/>
+  <v-row>
+    <v-col class="text-right">
+      Thanks to&nbsp;<a href="https://hotpot.ai/art-generator" target="_blank">hotpot.ai/art-generator</a>&nbsp;;-)
+    </v-col>
+  </v-row>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+p {
+  margin-bottom: 30px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="mb-4">
+        <p v-if="randomGegenstand.length > 0">
+          <strong>{{ randomGegenstand }}</strong>
+        </p>
+        <p v-else>
+          <strong>Es gibt noch keinen Gegensatz, musstu klickn ...</strong>
+        </p>
+        <p>
+          Ein Button:
+          <v-btn class="v-btn--rounded v-btn--border" color="blue-grey" v-on:click="machWas()">Gegenstand erzeugen
+          </v-btn>
+        </p>
+        <p>
+          So stellst man eine Variable dar (Doppel-Schweif-Klammern):
+          <br/>
+          {{ gegenstaende }}
+        </p>
+        <p>
+          {{ msg }}
+        </p>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+
+  props: ['msg'],
+
+  /* hier werden die variablen, die dargestellt werden sollen, definiert */
+  data: () => ({
+    textvar: 'lasdhkajhdkahdkjhaskd',
+    randomGegenstand: '',
+    gegenstaende: ['Topf', 'Nadel', 'Brat-Hühnchen', 'Pfanne', 'Tasse',],
+    eigennschaft: ['alt', 'neu', 'müffelt ein bisschen',],
+  }),
+  methods: {
+    machWas() {
+      console.log('Ich habe was gemacht, weil Du geklickt hast')
+      this.randomizeGegenstand()
+    },
+    randomizeGegenstand() {
+      /* "let" definiert auch Variablen, aber die sind lokal und werden nirgendwo dargestellt */
+      let randomNumber = this.getRandomIndexFromArray(this.gegenstaende)
+      this.randomGegenstand = this.gegenstaende[randomNumber]
+    },
+    getRandomIndexFromArray(dasArray) {
+      /* "let" definiert auch Variablen, aber die sind lokal und werden nirgendwo dargestellt */
+      let maxWert = dasArray.length
+      return Math.floor(Math.random() * maxWert)
+    },
+  }
+}
+</script>
