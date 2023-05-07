f9c84680dd55322f099feb86593eb5f9ea55dc8e
hoochicken <mareike.riegel@ql.de>
2023-05-07 18:30:09 +0200

    paraphenalia added

---
+++ my-app/src/components/RandomParaphenalia.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Random Paraphenalia</h2>
+    <random-element :items="paraphenalia" :buttonLabel="'Random Paraphenalia'"></random-element>
+  </div>
+</template>
+
+<script>
+import RandomElement from "@/components/RandomElement";
+export default {
+  name: "RandomParaphenalia",
+  components: {RandomElement},
+  data: () => {
+    return {
+      paraphenalia: [
+          'candle',
+          'skull of goat',
+          'oil',
+          'chrysalis',
+          'griffin\'s egg',
+          'basilisk\'s crown',
+          'wand',
+          'cubus',
+          'homunculus',
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- my-app/src/views/HomeView.vue
+++ my-app/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <!--HelloWorld msg="Das ist ein Property, die kann dann in der HelloWorld-Komponente dargestellt werden"/-->
     <random-ingredient></random-ingredient>
+    <random-paraphenalia></random-paraphenalia>
   </div>
 </template>
 
@@ -9,10 +10,12 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import RandomIngredient from "@/components/RandomIngredient";
+import RandomParaphenalia from "@/components/RandomParaphenalia.vue";
 
 export default {
   name: 'HomeView',
   components: {
+    RandomParaphenalia,
     // HelloWorld,
     RandomIngredient
   }
